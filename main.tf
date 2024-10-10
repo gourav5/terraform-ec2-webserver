2072daae0ad238ddf5ad30b8732215ed4f9d579f
gourav5 <33307086+gourav5@users.noreply.github.com>
2024-10-10 19:07:58 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,11 +2,14 @@
 # DEPLOY A SINGLE EC2 INSTANCE
 # ---------------------------------------------------------------------------------------------------------------------
 
-resource "aws_instance" "example" {
+resource "aws_instance" "prod-web" {
   # Ubuntu Server 18.04 LTS (HVM), SSD Volume Type in us-east-2
   ami                    = "ami-0c55b159cbfafe1f0"
   instance_type          = "t2.micro"
-  vpc_security_group_ids = [aws_security_group.instance.id]
+  key_name               = "devops"
+  subnet_id              = [aws_subnet.prod-subnet-pub.id]
+  vpc_security_group_ids = [aws_security_group.WEB_OUTSIDE_HTTP.id]
+  associate_public_ip_address = true
 
   user_data = <<-EOF
               #!/bin/bash
@@ -15,7 +18,7 @@ resource "aws_instance" "example" {
               EOF
 
   tags = {
-    Name = "terraform-example"
+    Name = "Prod-Webserver"
   }
 }
 
@@ -23,14 +26,14 @@ resource "aws_instance" "example" {
 # CREATE THE SECURITY GROUP THAT'S APPLIED TO THE EC2 INSTANCE
 # ---------------------------------------------------------------------------------------------------------------------
 
-resource "aws_security_group" "instance" {
-  name = "terraform-example-instance"
+#resource "aws_security_group" "instance" {
+ # name = "terraform-example-instance"
 
   # Inbound HTTP from anywhere
-  ingress {
-    from_port   = var.server_port
-    to_port     = var.server_port
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-}
+ # ingress {
+  #  from_port   = var.server_port
+   # to_port     = var.server_port
+   # protocol    = "tcp"
+   # cidr_blocks = ["0.0.0.0/0"]
+  #}
+#}
