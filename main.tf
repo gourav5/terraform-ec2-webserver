d150ce89aba45a3bbd3e6a285aea891117f440fa
gourav5 <33307086+gourav5@users.noreply.github.com>
2024-10-11 13:05:12 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -21,7 +21,8 @@ resource "aws_instance" "prod-web" {
                   sudo systemctl enable docker
                   sudo usermod -aG docker $USER
                   sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
-                  sudo chmod +x /usr/local/bin/docker-compose
+                  sudo chmod +x /usr/local/bin/docker-compose &
+                  EOF
 
 
 
